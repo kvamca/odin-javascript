fa243cb1d55ee1c1cea53be62afda43fc78ea164
kvamca <kvamca@gmail.com>
2024-03-10 20:06:03 +0100

    basic work with JS variables started

---
+++ javascript.js
@@ -0,0 +1,40 @@
+console.log("I was printed from external JS file")
+
+let message;
+message = "Hello!";
+//alert(message);
+
+
+let text = 'I am a declared variable';
+//alert(text);
+console.log(text)
+
+let aloha = "Aloha I was copied into another variable"
+let greetings;
+greetings = aloha;
+console.log(greetings)
+
+
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+console.log($ + _); // 3
+
+const myBirthday = '18.04.1982'; //const us UNCHANGING variable
+
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+console.log(color); // #FF7F00
+//capital-named constants are only used as aliases for “hard-to-remember” values
+
+
+let admin;
+let firstName;
+firstName = "John";
+admin = firstName;
+console.log(admin);
+
